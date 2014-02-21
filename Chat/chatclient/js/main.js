6e8602de3f2650f648fd8d4e04d1a3d771088230
Keli <thorkell@me.com>
2014-02-21 18:06:39 +0000

    got thiss

--- Chat/chatclient/js/main.js
+++ Chat/chatclient/js/main.js
@@ -17,14 +17,20 @@ function($scope, $http){
 	var socket = io.connect("http://localhost:8080");
 
 	socket.on("roomlist", function(data){
-		console.log(data);
+		$scope.$apply(function(){
+			$scope.rooms = data;	
+		});
+		
 	});
 
 	$scope.nick = "";
+	$scope.loggedIn = false;
+	$scope.rooms = [];
 	$scope.login = function(){
 		socket.emit("adduser", $scope.nick, function(available){
 			if(available){
 				//yay not taken
+				$scope.loggedIn = true;
 				socket.emit("rooms");
 			}
 		});
